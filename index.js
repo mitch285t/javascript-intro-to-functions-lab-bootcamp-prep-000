712e7b3a581685996c6823f183a1d11f110cdc9a
mitchell williams <mscottwilliams1994@gmail.com>
2019-06-08 19:37:12 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,7 +4,31 @@ function shout(string) {
 function whisper(string) {
   return string.toLowerCase()
 }
-function shoutlog(string) {
-  console.log.toUpperCase()
-  
+var uppercase = "HELLO"
+uppercase.toUpperCase() === uppercase 
+var lowercase = 'hello'
+lowercase.toLowerCase() === lowercase
+var mixedCase = 'Hi there!'
+mixedCase.toLowerCase() === mixedCase
+mixedCase.toUpperCase() === mixedCase
+function logShout(string) {
+  console.log(uppercase)
 }
+function logWhisper(string) {
+  console.log(lowercase)
+}
+
+
+
+
+function sayHiToGrandma(string) {
+ 
+ if (string.toLowerCase() === string) {
+   return "I can't hear you!"
+ }  
+  if (string.toUpperCase()=== string) {
+    return "YES INDEED!"
+  }
+  if (string === "I love you, Grandma.") {
+    return "I love you, too."
+  }}
